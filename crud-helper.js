10eef908bd57d690cefc262ff297ff0a58fd9abf
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-24 18:29:09 -0400

    fixed export error on database.js and added src file structure

--- crud-helper.js
+++ crud-helper.js
@@ -5,10 +5,10 @@ require('./config/database');
 
 // Require the Mongoose models
 // const User = require('./models/user');
-// const Item = require('./models/item');
+// const Item = require('./models/exercise');
 // const Category = require('./models/category');
-// const Order = require('./models/order');
+// const Order = require('./models/entry');
 
 // Local variables will come in handy for holding retrieved documents
-let user, item, category, order;
-let users, items, catgories, orders;
+let user, exercise, category, entry;
+let users, exercises, catgories, entries;
