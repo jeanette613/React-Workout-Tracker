5caeba9ed4c44d7abef97bfc06c9a54e5deb0b3a
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-24 23:27:56 -0400

    input field for sign up form added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
-import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router><App /></Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
