2e732b7fbc4c63aab1d4e4b5c177a344841af115
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-24 23:45:17 -0400

    handleChange for input fields added

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -8,6 +8,12 @@ export default class SignUpForm extends Component {
         confirm: '',
         error: ''
     }
+
+    handleChange = (evt) => {
+        this.setState({ ...this.state, [evt.target.name]: evt.target.value })
+
+    }
+
     render() {
         const disable = this.state.password !== this.state.confirm;
         return (
